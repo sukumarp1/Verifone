4d54035bf20a17b80e4912c7fd2f70f4bff7bf6d
P Sukumar <28951@ITCINFOTECH.COM>
2020-07-30 17:39:38 +0530

    Implemented download functionality

--- src/app/components/favorite/favorite.component.ts
+++ src/app/components/favorite/favorite.component.ts
@@ -15,7 +15,6 @@ export class FavoriteComponent implements OnInit {
 
   ngOnInit() {
     this.helper.favoriteImages$.subscribe(result => {
-
       this.favoriteImages = result;
       if (result != undefined) {
         this.favoriteImages = result;
@@ -24,5 +23,28 @@ export class FavoriteComponent implements OnInit {
         this.router.navigate(['search'])
       }
     });
+  }
+  downloadImage(obj) {
+    let splitbyte = obj.thumbnail.split(',');
+    let byteCharacters = atob(splitbyte[1]);
+    let byteNumbers = new Array(byteCharacters.length);
+    for (var i = 0; i < byteCharacters.length; i++) {
+      byteNumbers[i] = byteCharacters.charCodeAt(i);
+    }
+    var byteArray = new Uint8Array(byteNumbers);
+    let blob = new Blob([byteArray], { "type": "image/jpeg" });
+    if (navigator.msSaveBlob) {
+      let filename = obj.name;
+      navigator.msSaveBlob(blob, filename);
+    } else {
+
+      let link = document.createElement("a");
+      link.href = URL.createObjectURL(blob);
+      link.setAttribute('visibility', 'hidden');
+      link.download = obj.name;
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    }
   }
 }
